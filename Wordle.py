e1ac19b32031c13ad58f497d9b2b54e934686177
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-03-26 09:59:07 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,13 +1,13 @@
 import random
 
-world_list = []
+word_list = []
 
 with open('wordle-allowed-guesses.txt') as file:
     for line in file:
-        world_list.append(line)
+        word_list.append(line)
 file.close()
 
-answer = random.choice(world_list)
+answer = random.choice(word_list)
 print(answer) #for testing
 in_word = []
 not_in_word = []
@@ -27,17 +27,17 @@ def Check_correct_positions(x):
             not_in_word.append(letter)
         position += 1
  
-def Check_valid_answer(x):
-    return x in world_list
+def Check_valid_guess(x):
+    return x in word_list
 
 win = False
 guess_count = 1
 while guess_count <= 6 and win == False:
-    valid_answer = False
-    while valid_answer == False:
+    valid_guess = False
+    while valid_guess == False:
         guess = input('Enter guess #'+ str(guess_count) + ' >> ')
-        if Check_answer(guess):
-            valid_answer = True
+        if Check_valid_guess(guess):
+            valid_guess = True
         else:
             print('Invalid answer. Guess again.')
     if Check_answer(guess):
@@ -47,7 +47,7 @@ while guess_count <= 6 and win == False:
     print('Correct letters so far:' + str(correct_letter))
     print('Letters in word:' + str(in_word))
     print('Letters not in word:' + str(not_in_word))
-    guess_count +=1
+    guess_count += 1
 if win == True:
     print ('You win.')
 else:
