097ce7da1653afb75533d0919cdaccf3a6cce90b
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-03-26 00:04:39 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -27,21 +27,24 @@ def Check_correct_positions(x):
         if letter not in in_word:
             in_word_checked.append(letter)
     in_word = in_word_checked
+def Check_valid_answer(x):
+    return x == world_list
+
+Win = False
+guess_count = 0
+while guess_count <= 6 and Win == False:
+    Guess = input('Enter first guess >> ')
+    if Check_answer(Guess) == True:
+        Win = True
+    else:
+        Check_correct_positions(Guess)
+    print('Correct letters so far:' + correct_letter)
+    print('Letters in word:' + in_word)
+    guess_count +=1
+if win == True:
+    print ('You win.')
+else:
+    print ('You Lose. Answer was ' + answer + '.')
 
 
-Guess1 = input('Enter first guess >> ')
-if Check_answer(Guess1) == True:
-    print ('You win!')
-
-Check_correct_positions(Guess1)
-
-Guess2 = input('Enter first guess >> ')
-
-Guess3 = input('Enter first guess >> ')
-
-Guess4 = input('Enter first guess >> ')
-
-Guess5 = input('Enter first guess >> ')
-
-Guess6 = ('Enter first guess >> ')
 
